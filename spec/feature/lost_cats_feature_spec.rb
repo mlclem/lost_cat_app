3af2fc48126d87c321508e4fead34f7dcc83643e
Matt Clements <matthewlclements@gmail.com>
2022-05-19 15:11:37 +0100

    Minor HTML changes

--- spec/feature/lost_cats_feature_spec.rb
+++ spec/feature/lost_cats_feature_spec.rb
@@ -10,11 +10,11 @@ RSpec.describe "Lost cats feature tests", type: :feature do
       click_link "Add a cat"
       fill_in "name", with: "Gertrude"
       fill_in "phone", with: "011121 123 456"
-      fill_in "description", with: "A large white cat who appears to be deaf (It meows very loudly)"
+      fill_in "description", with: "A large white cat who appears to be deaf (They meows very loudly)"
       click_button "Submit"
       expect(page).to have_content "Gertrude"
       expect(page).to have_content "011121 123 456"
-      expect(page).to have_content "A large white cat who appears to be deaf (It meows very loudly)"
+      expect(page).to have_content "A large white cat who appears to be deaf (They meow very loudly)"
     end
 
     it "adds multiple lost cats and reports them" do
@@ -22,7 +22,7 @@ RSpec.describe "Lost cats feature tests", type: :feature do
       click_link "Add a cat"
       fill_in "name", with: "Gertrude"
       fill_in "phone", with: "011121 123 456"
-      fill_in "description", with: "A large white cat who appears to be deaf (It meows very loudly)"
+      fill_in "description", with: "A large white cat who appears to be deaf (They meow very loudly)"
       click_button "Submit"
       click_link "Add a cat"
       fill_in "name", with: "The Baron"
@@ -36,13 +36,20 @@ RSpec.describe "Lost cats feature tests", type: :feature do
       click_button "Submit"
       expect(page).to have_content "Gertrude"
       expect(page).to have_content "011121 123 456"
-      expect(page).to have_content "A large white cat who appears to be deaf (It meows very loudly)"
+      expect(page).to have_content "A large white cat who appears to be deaf (They meow very loudly)"
       expect(page).to have_content "The Baron"
       expect(page).to have_content "09875 456 321"
       expect(page).to have_content "A cat wearing a suit and tophat"
       expect(page).to have_content "Salem"
       expect(page).to have_content "066606 666 666"
       expect(page).to have_content "A snarky black cat who knows magic"
+    end
+  end
+
+  context "If there are existing lost cats" do
+    it "A can instance can be updated" do
+    
+    
     end
   end
 end
